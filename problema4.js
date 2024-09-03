7e6be2126cfcda6caaac8ce1cc1826fe450f10db
Luan Alberti <144260420+Luan-Alberti@users.noreply.github.com>
2024-09-03 13:15:23 -0300

    Update problema4.js

--- problema4.js
+++ problema4.js
@@ -6,9 +6,11 @@ const faturamentoEstados = {
     Outros: 19849.53
 }; // Valores de faturamento por estado
 
-const totalFaturamento = Object.values(faturamentoEstados).reduce((total, valor) => total + valor, 0); // Calcula o valor total de faturamento somando os valores de todos os estados
+const totalFaturamento = Object.values(faturamentoEstados).reduce((total, valor) => total + valor, 0); 
+// Object.values(faturamentoEstados): Esse método extrai os valores do objeto faturamentoEstados e os coloca em um array.
+// reduce((total, valor) => total + valor, 0): reduce é um método de array que executa uma função em cada elemento do array para reduzir o array a um único valor, basicamente calculando uma soma tos valores.
 
-for (let estado in faturamentoEstados) {
-    let percentual = (faturamentoEstados[estado] / totalFaturamento) * 100;
-    console.log(`${estado}: ${percentual.toFixed(2)}%`);
-}  // Calcula e exibe o percentual de representação de cada estado
+for (let estado in faturamentoEstados) { //  Esse loop percorre cada chave (estado) no objeto faturamentoEstados.
+    let percentual = (faturamentoEstados[estado] / totalFaturamento) * 100; //  Divide o valor do faturamento de um estado pelo totalFaturamento para obter a fração que esse estado representa do total, e multiplica por 100 para converter em percentual.
+    console.log(`${estado}: ${percentual.toFixed(2)}%`); 
+}  // Exibe o nome do estado e seu percentual correspondente no console, usando 2 casa decimais
